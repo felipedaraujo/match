52aaf1ee6b00a63a07f777970d82af0a83035c86
Felipe de Araujo <oifelipearaujo@gmail.com>
2016-01-27 23:37:24 -0600

    Add settings & levels

---
+++ js/settingsController.js
@@ -0,0 +1,10 @@
+angular.module('starter.controllers').
+  controller('SettingsCtrl', function($scope, $http, $cordovaFile) {
+    $scope.level = window.localStorage['level'] || "Easy";
+    $scope.sound = window.localStorage['sound'] || "On";
+
+    $scope.toggleSetting = function(name, value) {
+      window.localStorage[name] = value;
+    }
+
+  })
