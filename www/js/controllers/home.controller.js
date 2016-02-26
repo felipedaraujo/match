ed94ebe485f06b0450c54ffe3441ea7228dea75f
Felipe de Araujo <oifelipearaujo@gmail.com>
2016-02-26 11:15:23 -0600

    Finish share function

--- www/js/controllers/home.controller.js
+++ www/js/controllers/home.controller.js
@@ -1,6 +1,21 @@
 angular.module('starter.controllers').
-  controller('HomeCtrl', function($scope, $location) {
+  controller('HomeCtrl', function($scope, $location, $cordovaSocialSharing) {
     $scope.go = function ( path ) {
       $location.path( path );
     };
-  })
+
+    $scope.share = function() {
+      var message = 'Tres is a card game designed to put your brain to the test! Make as many Tres sets as you...';
+      var subject = 'Have you heard about Tres Card Game?';
+      var file = 'www/img/Tres-Card-Game.png';
+      var link = 'http://bit.ly/1QKWkPQ';
+
+      $cordovaSocialSharing
+        .share(message, subject, file, link)
+        .then(function(result) {
+          // Success!
+        }, function(err) {
+          // An error occured. Show a message to the user
+        });
+    }
+})
