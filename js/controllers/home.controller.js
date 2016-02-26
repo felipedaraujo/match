90222e0e3a72e8d36c5c46b2fa088000f4b78437
Felipe de Araujo <oifelipearaujo@gmail.com>
2016-02-26 16:24:06 -0600

    Fix position of menu button

--- js/controllers/home.controller.js
+++ js/controllers/home.controller.js
@@ -7,7 +7,7 @@ angular.module('starter.controllers').
     $scope.share = function() {
       var message = 'Tres is a card game designed to put your brain to the test! Make as many Tres sets as you...';
       var subject = 'Have you heard about Tres Card Game?';
-      var file = 'www/img/Tres-Card-Game.png';
+      var file = 'www/img/tres-card-game.png';
       var link = 'http://bit.ly/1QKWkPQ';
 
       $cordovaSocialSharing
